2ae16c0cd9a43eb37db29d4b90db14b3bf7ad2f7
Nikolay Lukhtura <rrwniko@gmail.com>
2022-07-14 16:53:13 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 let searchByID = function () {
     const INPUT = document.querySelector('.input');
-    const promise = new Promise(function (resolve, reject) {
-        const SUBMIT = document.querySelector('.submit').addEventListener('click', function (e) {
-            e.preventDefault();
-            if (INPUT.value >= 1 && INPUT.value <= 100) {
-                fetch(`https://jsonplaceholder.typicode.com/posts/${INPUT.value}`)
-                    .then(response => response.json())
-                    .then(json => {
-                        RESULT_WINDOW.innerHTML = JSON.stringify(json);
-                        resolve();
-                    });
-            };
-        });
-        const RESULT_WINDOW = document.querySelector('.result-output');
-    });
-
-    promise.then(() => {
-        const COMMENTS_WINDOW = document.querySelector('.comments-output')
-        fetch(`https://jsonplaceholder.typicode.com/posts/${INPUT.value}/comments`)
+    const COMMENTS_WINDOW = document.querySelector('.comments-output');
+    const RESULT_WINDOW = document.querySelector('.result-output');
+    const SUBMIT = document.querySelector('.submit').addEventListener('click', function (e) {
+        e.preventDefault();
+        if (INPUT.value < 1 || INPUT.value > 100) {
+            RESULT_WINDOW.innerHTML = '<p>Something went wrong</p>';
+            COMMENTS_WINDOW.innerHTML = '<p>Something went wrong</p>';
+            throw new Error('Something went wrong');  
+        }
+        fetch(`https://jsonplaceholder.typicode.com/posts/${INPUT.value}`)
             .then(response => response.json())
             .then(json => {
-                COMMENTS_WINDOW.innerHTML = JSON.stringify(json);
+                RESULT_WINDOW.innerHTML = JSON.stringify(json)
+            })
+            .then(() => {
+                fetch(`https://jsonplaceholder.typicode.com/posts/${INPUT.value}/comments`)
+                    .then(response => response.json())
+                    .then(json => {
+                        COMMENTS_WINDOW.innerHTML = JSON.stringify(json);
+                    });
             });
     });
+
 };
 searchByID();
